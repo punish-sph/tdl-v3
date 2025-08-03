8fd9f86921696b23855d47995ae9ddfe2a539f9f
punisher-sph <arkadanisaysstuff@gmail.com>
2025-08-03 21:04:08 +0700

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+   
+  </React.StrictMode>,
+)
+
+window.ipcRenderer.on('main-process-message', (_event, message) => {
+  console.log(message)
+})
